bd655ffa664a5f5b261d1e8e88cc130600c8c92a
Theodor Har <theodorlehar@gmail.com>
2021-06-19 17:50:58 -0700

    wipe

--- Dockerfile
+++
@@ -1,2 +0,0 @@
-FROM php:8.0.3-apache
-RUN docker-php-ext-install mysqli
--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-session_destroy(); 
-?>
-<!DOCTYPE HTML>
-
-<script type='text/javascript'>
-	window.location.replace("index.php");
-</script>
