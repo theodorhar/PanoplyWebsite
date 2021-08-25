4ffd7d8d9a8d1c7ad63a2fa94c5294afb78737a7
Theodor Har <theodorlehar@gmail.com>
2021-08-25 11:53:10 -0700

    add profile picture

--- components/common/Avatar/Avatar.tsx
+++ components/common/Avatar/Avatar.tsx
@@ -1,5 +1,6 @@
 import { FC, useRef, useEffect } from 'react'
 import { useUserAvatar } from '@lib/hooks/useUserAvatar'
+import { useUser } from '@auth0/nextjs-auth0'
 
 interface Props {
   className?: string
@@ -9,14 +10,20 @@ interface Props {
 const Avatar: FC<Props> = ({}) => {
   let ref = useRef() as React.MutableRefObject<HTMLInputElement>
   let { userAvatar } = useUserAvatar()
-
+  const { user, error, isLoading } = useUser()
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>{error.message}</div>
   return (
     <div
       ref={ref}
       style={{ backgroundImage: userAvatar }}
       className="inline-block h-8 w-8 rounded-full border-2 border-primary hover:border-secondary focus:border-secondary transition linear-out duration-150"
     >
-      {/* Add an image - We're generating a gradient as placeholder  <img></img> */}
+      <img
+        alt="Profile Picture"
+        src={user.picture}
+        style={{ borderRadius: 50 }}
+      />
     </div>
   )
 }
