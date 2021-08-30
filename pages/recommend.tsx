049c4f55e3b64625bbda2ddf8ef3864fdecac127
Theodor Har <theodorlehar@gmail.com>
2021-08-30 13:10:51 -0700

    lay out skeleton for recommend

--- pages/recommend.tsx
+++ pages/recommend.tsx
@@ -1,8 +1,29 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import { Layout } from '@components/common'
-import { Container, Text } from '@components/ui'
+import { RecipeCard } from '@components/product'
+import { Container, Text, Grid, Skeleton } from '@components/ui'
+import axios from 'axios'
+import rangeMap from '@lib/range-map'
 
-export default function Profile({ user }) {
+export const getServerSideProps = withPageAuthRequired({
+  async getServerSideProps(context) {
+    axios.defaults.baseURL = process.env.REACT_APP_API_URL
+    const res = await axios.get('/default', {
+      //change this to /recommend
+      headers: {
+        //'Authorization':
+      },
+    })
+    const results = JSON.parse(JSON.stringify(res.data))
+    return {
+      props: {
+        results,
+      },
+    }
+  },
+})
+
+export default function Recommend({ user, results }) {
   return (
     <Container>
       <Text variant="pageHeading">Recommendations</Text>
@@ -20,9 +41,30 @@ export default function Profile({ user }) {
           </div>
         </div>
       )}
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-4 mt-3 mb-20">
+        {/* Products */}
+        <div className="col-span-8 order-3 lg:order-none">
+          {results ? (
+            <Grid layout="normal">
+              {results.map((recipe) => (
+                <RecipeCard key={recipe.id} recipe={recipe} variant="slim" />
+              ))}
+            </Grid>
+          ) : (
+            <Grid layout="normal">
+              {rangeMap(30, (i) => (
+                <Skeleton
+                  key={i}
+                  className="w-full animated fadeIn"
+                  height={325}
+                />
+              ))}
+            </Grid>
+          )}
+        </div>
+      </div>
     </Container>
   )
 }
-export const getServerSideProps = withPageAuthRequired()
 
-Profile.Layout = Layout
+Recommend.Layout = Layout
